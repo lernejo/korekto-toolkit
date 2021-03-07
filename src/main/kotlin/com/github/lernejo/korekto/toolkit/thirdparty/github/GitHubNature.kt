b2d262c10a29fad40d18049e967b505ae60a2c46
ledoyen <ledoyen.loic@gmail.com>
2021-03-07 10:13:14 +0100

    Make GitHub a singleton and add support for listing actions runs

--- src/main/kotlin/com/github/lernejo/korekto/toolkit/thirdparty/github/GitHubNature.kt
+++ src/main/kotlin/com/github/lernejo/korekto/toolkit/thirdparty/github/GitHubNature.kt
@@ -4,6 +4,7 @@ import com.github.lernejo.korekto.toolkit.Exercise
 import com.github.lernejo.korekto.toolkit.Nature
 import com.github.lernejo.korekto.toolkit.NatureContext
 import com.github.lernejo.korekto.toolkit.NatureFactory
+import com.google.gson.Gson
 import okhttp3.OkHttpClient
 import org.eclipse.jgit.api.Git
 import org.kohsuke.github.GHRepository
@@ -11,8 +12,29 @@ import org.kohsuke.github.GitHub
 import org.kohsuke.github.GitHubBuilder
 import org.kohsuke.github.extras.okhttp3.OkHttpConnector
 import java.io.IOException
+import java.net.HttpURLConnection
+import java.net.URL
+import java.nio.charset.StandardCharsets
 import java.util.*
 import java.util.concurrent.TimeUnit
+
+internal object GitHubClientHolder {
+    val client: GitHub by lazy {
+        val builder = GitHubBuilder()
+            .withConnector(
+                OkHttpConnector(
+                    OkHttpClient.Builder()
+                        .readTimeout(2L, TimeUnit.SECONDS)
+                        .build()
+                )
+            )
+        val token = System.getProperty("github_token")
+        if (token != null) {
+            builder.withOAuthToken(token)
+        }
+        builder.build()
+    }
+}
 
 class GitHubNature(val context: GitHubContext) : Nature<GitHubContext> {
     override fun <RESULT> withContext(action: (GitHubContext) -> RESULT): RESULT = action.invoke(context)
@@ -20,9 +42,46 @@ class GitHubNature(val context: GitHubContext) : Nature<GitHubContext> {
     override fun finalize() {
         super.finalize()
     }
+
+    fun listActionRuns(): List<WorkflowRun> {
+        val requestURL = "https://api.github.com/repos/${context.exerciseName}/actions/runs"
+        val url = URL(requestURL)
+        val conn: HttpURLConnection = url.openConnection() as HttpURLConnection
+        val token = System.getProperty("github_token")
+        if (token != null) {
+            conn.setRequestProperty("Authorization", "token $token");
+        }
+
+        return conn.inputStream.use { `is` ->
+            Scanner(`is`, StandardCharsets.UTF_8).use { scanner ->
+                val jsonString = scanner.useDelimiter("\\A").next()
+                Gson().fromJson(jsonString, ActionRunsResponse::class.java).workflow_runs
+            }
+        }
+    }
 }
 
-class GitHubContext(val gitHub: GitHub, val repository: GHRepository) : NatureContext
+data class ActionRunsResponse(val workflow_runs: List<WorkflowRun>)
+data class WorkflowRun(
+    val name: String,
+    val head_branch: String,
+    val status: WorkflowRunStatus,
+    val conclusion: WorkflowRunConclusion?
+)
+
+enum class WorkflowRunStatus {
+    queued, in_progress, completed
+}
+
+enum class WorkflowRunConclusion {
+    action_required, cancelled, failure, neutral, success, skipped, stale, timed_out
+}
+
+class GitHubContext(val gitHub: GitHub, val exerciseName: String) : NatureContext {
+    val repository: GHRepository by lazy {
+        gitHub.getRepository(exerciseName)
+    }
+}
 
 class GitHubNatureFactory : NatureFactory {
     override fun getNature(exercise: Exercise): Optional<Nature<*>> {
@@ -33,19 +92,8 @@ class GitHubNatureFactory : NatureFactory {
                 .filter { uri -> uri.isRemote }
                 .any { uri -> "github.com" == uri.host }
             git.close()
-            val token = System.getProperty("github_token")
-            if (gitHubRemote && token != null) {
-                val gitHub = GitHubBuilder()
-                    .withOAuthToken(token)
-                    .withConnector(
-                        OkHttpConnector(
-                            OkHttpClient.Builder()
-                                .readTimeout(2L, TimeUnit.SECONDS)
-                                .build()
-                        )
-                    )
-                    .build()
-                Optional.of(GitHubNature(GitHubContext(gitHub, gitHub.getRepository(exercise.name))))
+            if (gitHubRemote) {
+                Optional.of(GitHubNature(GitHubContext(GitHubClientHolder.client, exercise.name)))
             } else {
                 Optional.empty()
             }
