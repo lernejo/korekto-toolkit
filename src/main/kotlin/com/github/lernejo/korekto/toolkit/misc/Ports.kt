9a9c02768d7f81de0ca3b70d000ff7f2b6a1b5fb
ledoyen <ledoyen.loic@gmail.com>
2021-03-23 18:31:24 +0100

    Add port utility

---
+++ src/main/kotlin/com/github/lernejo/korekto/toolkit/misc/Ports.kt
@@ -0,0 +1,34 @@
+package com.github.lernejo.korekto.toolkit.misc
+
+import java.io.IOException
+import java.net.Socket
+import java.util.concurrent.CancellationException
+import java.util.concurrent.TimeUnit
+
+object Ports {
+    @JvmStatic
+    fun waitForPortToBeListenedTo(port: Int, timeoutUnit: TimeUnit, timeout: Long) {
+        val timeoutInMs = TimeUnit.MILLISECONDS.convert(timeout, timeoutUnit)
+        val startTime = System.currentTimeMillis()
+        do {
+            if (isListened(port)) {
+                return
+            }
+            try {
+                TimeUnit.MILLISECONDS.sleep(50L)
+            } catch (e: InterruptedException) {
+                throw IllegalStateException(e)
+            }
+        } while (System.currentTimeMillis() - startTime < timeoutInMs)
+        throw CancellationException()
+    }
+
+    @JvmStatic
+    fun isListened(port: Int): Boolean {
+        try {
+            Socket("localhost", port).use { return false }
+        } catch (ignored: IOException) {
+            return true
+        }
+    }
+}
