3ec1dc2c3c415877baa3709662c69ef05bdc5613
ledoyen <ledoyen.loic@gmail.com>
2021-11-30 21:29:35 +0100

    Fix failing UT

--- src/test/kotlin/com/github/lernejo/korekto/toolkit/thirdparty/git/ExerciseClonerTest.kt
+++ src/test/kotlin/com/github/lernejo/korekto/toolkit/thirdparty/git/ExerciseClonerTest.kt
@@ -1,10 +1,12 @@
 package com.github.lernejo.korekto.toolkit.thirdparty.git
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty
 import java.nio.file.Paths
 
 internal class ExerciseClonerTest {
     @Test
+    @EnabledIfSystemProperty(named = "github_token", matches = ".+")
     internal fun sample_clone() {
         var ex = ExerciseCloner(Paths.get("target/repositories")).gitClone(
             "https://github.com/lernejo/git_training"
