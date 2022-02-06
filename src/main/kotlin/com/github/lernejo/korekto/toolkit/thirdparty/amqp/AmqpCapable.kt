a2c2804a0ee6a845e61284d70577003c44ded413
ledoyen <ledoyen.loic@gmail.com>
2022-02-06 23:36:06 +0100

    Add queueExists utility for AMQP

--- src/main/kotlin/com/github/lernejo/korekto/toolkit/thirdparty/amqp/AmqpCapable.kt
+++ src/main/kotlin/com/github/lernejo/korekto/toolkit/thirdparty/amqp/AmqpCapable.kt
@@ -1,12 +1,13 @@
 package com.github.lernejo.korekto.toolkit.thirdparty.amqp
 
+import com.rabbitmq.client.Connection
 import com.rabbitmq.client.ConnectionFactory
 import java.io.IOException
 import java.util.concurrent.TimeoutException
 
 interface AmqpCapable {
 
-    fun deleteQueue(factory: ConnectionFactory, queueName: String?) {
+    fun deleteQueue(factory: ConnectionFactory, queueName: String) {
         try {
             factory.newConnection().use {
                 it.createChannel().use {
@@ -17,6 +18,29 @@ interface AmqpCapable {
             throw IllegalStateException("Could not connect to RabbitMQ at " + factory.host + ":" + factory.port, e)
         } catch (e: TimeoutException) {
             throw IllegalStateException("Could not connect to RabbitMQ at " + factory.host + ":" + factory.port, e)
+        }
+    }
+
+    fun doesQueueExists(factory: ConnectionFactory, queueName: String): Boolean {
+        try {
+            factory.newConnection().use {
+                return doesQueueExists(it, queueName)
+            }
+        } catch (e: IOException) {
+            throw IllegalStateException("Could not connect to RabbitMQ at " + factory.host + ":" + factory.port, e)
+        } catch (e: TimeoutException) {
+            throw IllegalStateException("Could not connect to RabbitMQ at " + factory.host + ":" + factory.port, e)
+        }
+    }
+
+    fun doesQueueExists(connection: Connection, queueName: String): Boolean {
+        try {
+            connection.createChannel().use { channel ->
+                val declareOk = channel.queueDeclarePassive(queueName)
+                return declareOk != null
+            }
+        } catch (e: IOException) {
+            return false
         }
     }
 }
