cc205aed055f0d8c4fc2ca01347d4de255866f56
ledoyen <ledoyen.loic@gmail.com>
2024-09-17 02:36:40 +0200

    Handle missing permission to access GitHub actions API

--- src/main/kotlin/com/github/lernejo/korekto/toolkit/partgrader/GitHubActionsPartGrader.kt
+++ src/main/kotlin/com/github/lernejo/korekto/toolkit/partgrader/GitHubActionsPartGrader.kt
@@ -7,6 +7,7 @@ import com.github.lernejo.korekto.toolkit.thirdparty.git.GitNature
 import com.github.lernejo.korekto.toolkit.thirdparty.github.GitHubNature
 import com.github.lernejo.korekto.toolkit.thirdparty.github.WorkflowRunConclusion
 import com.github.lernejo.korekto.toolkit.thirdparty.github.WorkflowRunStatus
+import java.io.IOException
 import java.util.*
 
 class GitHubActionsPartGrader<T : GradingContext>(
@@ -23,27 +24,34 @@ class GitHubActionsPartGrader<T : GradingContext>(
         if (gitHubNature.isEmpty) {
             return result(listOf("Not a GitHub project"), 0.0)
         }
-        val actionRuns = gitHubNature.get().listActionRuns()
+        try {
+            val actionRuns = gitHubNature.get().listActionRuns()
 
-        val currentBranchRuns = actionRuns
-            .filter { ar -> ar.status === WorkflowRunStatus.completed }
-            .filter { ar -> ar.head_branch == branch }
+            val currentBranchRuns = actionRuns
+                .filter { ar -> ar.status === WorkflowRunStatus.completed }
+                .filter { ar -> ar.head_branch == branch }
 
-        return if (currentBranchRuns.isEmpty()) {
-            result(
-                listOf("No CI runs for `$branch` branch, check https://github.com/${context.exercise?.name}/actions"),
-                0.0
-            )
-        } else {
-            val conclusion = currentBranchRuns[0].conclusion
-            if (conclusion !== WorkflowRunConclusion.success) {
+            return if (currentBranchRuns.isEmpty()) {
                 result(
-                    listOf("Latest CI run of branch `$branch` was expected to be in *success* state but found: $conclusion"),
-                    maxGrade() / 2
+                    listOf("No CI runs for `$branch` branch, check https://github.com/${context.exercise?.name}/actions"),
+                    0.0
                 )
             } else {
-                result(listOf(), maxGrade())
+                val conclusion = currentBranchRuns[0].conclusion
+                if (conclusion !== WorkflowRunConclusion.success) {
+                    result(
+                        listOf("Latest CI run of branch `$branch` was expected to be in *success* state but found: $conclusion"),
+                        maxGrade() / 2
+                    )
+                } else {
+                    result(listOf(), maxGrade())
+                }
             }
+        } catch (e: IOException) {
+            return result(
+                listOf("Missing permission to access GitHub actions read-only API"),
+                0.0
+            )
         }
     }
 }
